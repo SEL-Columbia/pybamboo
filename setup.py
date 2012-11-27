9a6df4b1a201e0d21f92b1c2ce136c00a2af9706
renaud gaudin <rgaudin@gmail.com>
2012-11-27 18:52:52 +0000

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
      name='pybamboo',
-     version='0.5.2',
+     version='0.5.3',
      author='modilabs',
      author_email='info@modilabs.org',
      packages=['pybamboo'],
@@ -13,5 +13,6 @@ setup(
      install_requires=[
         'requests==0.14.0',
         'simplejson==2.6.2',
+        'bson==0.3.3'
     ],
 )
