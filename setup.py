609409e83cfc3cb8f8b10ca04a0bf639c372fe55
renaud gaudin <rgaudin@gmail.com>
2013-03-07 16:29:54 +0000

    Added rolling endpoint support + pep8 on setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from distutils.core import setup
 
 setup(
-     name='pybamboo',
-     version='0.5.7.7',
-     author='modilabs',
-     author_email='info@modilabs.org',
-     packages=['pybamboo'],
-     package_dir={'pybamboo': 'pybamboo'},
-     url='http://pypi.python.org/pypi/pybamboo/',
-     description='A Python package to interact with bamboo.io.',
-     long_description=open('README.rst', 'rt').read(),
-     install_requires=[
+    name='pybamboo',
+    version='0.5.7.7',
+    author='modilabs',
+    author_email='info@modilabs.org',
+    packages=['pybamboo'],
+    package_dir={'pybamboo': 'pybamboo'},
+    url='http://pypi.python.org/pypi/pybamboo/',
+    description='A Python package to interact with bamboo.io.',
+    long_description=open('README.rst', 'rt').read(),
+    install_requires=[
         'requests==0.14.0',
         'simplejson==2.6.2',
         'pymongo==2.3'
